01b81c8e648bfc5693492d2da4ea1c5fa4b8b43a
G7Bavv <g7bprogamer@gmail.com>
2025-11-10 13:27:07 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js-selfbot-v13"); 
+const client = new Discord.Client({
+    intents: [Discord.Intents.FLAGS.GUILDS]
+});
+client.on('ready', async () => {
+  console.log(`${client.user.username} is ready!`);
+})
+const {userAccount} = require("sphinx-run");
+new userAccount(client, Discord).leveling({
+    channel: '1436358636032102526',
+    randomLetters: false, 
+    time: 10000, 
+    type: 'eng' 
+});
+client.login("token");
